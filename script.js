6ad25ecc65b3506e7e6cf2bcf92c3a7eea5d7d20
eijoeljunior289 <joeljunior289@outlook.pt>
2024-12-29 15:49:43 -0300

    script.js

--- script.js
+++ script.js
@@ -119,3 +119,19 @@ function estourarFogos() {
 }
 
 // Fogos
+
+// Script para o botão de voltar ao topo
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopButton.style.display = 'block';
+    } else {
+        scrollToTopButton.style.display = 'none';
+    }
+});
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+// Script para o botão de voltar ao topo
